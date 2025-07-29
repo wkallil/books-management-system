ec70e0076665d9f9f040f93efbf9335ed661b43f
wkallil <kallil.tim@gmail.com>
2025-07-29 16:51:33 -0300

    refactor: switching logger to slf4j package

--- src/main/java/br/com/wkallil/services/PersonServices.java
+++ src/main/java/br/com/wkallil/services/PersonServices.java
@@ -4,18 +4,17 @@ package br.com.wkallil.services;
 import br.com.wkallil.exceptions.ResourceNotFoundException;
 import br.com.wkallil.models.Person;
 import br.com.wkallil.repositories.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.Logger;
 
 @Service
 public class PersonServices {
 
-    private final AtomicLong counter = new AtomicLong();
-    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+    private final Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
 
     @Autowired
     PersonRepository repository;
