ff44cbdda8110e3e5016c32b86d94b2db9f15d7e
wkallil <kallil.tim@gmail.com>
2025-07-29 18:35:25 -0300

    feat: created PersonDTO and PersonMapper with MapStruct

---
+++ src/main/java/br/com/wkallil/mapper/PersonMapper.java
@@ -0,0 +1,21 @@
+package br.com.wkallil.mapper;
+
+import br.com.wkallil.data.dto.PersonDTO;
+import br.com.wkallil.models.Person;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface PersonMapper {
+
+
+    PersonDTO toDto(Person person);
+
+    Person toEntity(PersonDTO personDTO);
+
+    List<PersonDTO> toDtoList(List<Person> peopleList);
+
+    List<Person> toEntityList(List<PersonDTO> peopleDTOList);
+
+}
