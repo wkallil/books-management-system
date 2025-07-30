40b2abcfd028a2c856197f3a26cf553aa12f60ae
wkallil <kallil.tim@gmail.com>
2025-07-30 15:46:08 -0300

    feat: implementing flyway

---
+++ src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -0,0 +1,11 @@
+-- books_management_system.person definition
+
+CREATE TABLE IF NOT EXISTS `person` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(80) NOT NULL,
+  `last_name` varchar(80) NOT NULL,
+  `address` varchar(150) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
---
+++ src/main/resources/db/migration/V2__Insert_Table_Person.sql
@@ -0,0 +1,11 @@
+INSERT INTO person (address, first_name, gender, last_name) VALUES
+('Rua das Flores, 123', 'Ana', 'Female', 'Silva'),
+('Avenida Principal, 456', 'Bruno', 'Male', 'Santos'),
+('Travessa da Paz, 789', 'Carla', 'Female', 'Oliveira'),
+('Praça da Liberdade, 101', 'Daniel', 'Male', 'Ferreira'),
+('Estrada do Sol, 202', 'Eliana', 'Female', 'Pereira'),
+('Rua Nova, 303', 'Fábio', 'Male', 'Rodrigues'),
+('Alameda dos Sonhos, 404', 'Gabriela', 'Female', 'Almeida'),
+('Beco da Alegria, 505', 'Hugo', 'Male', 'Lima'),
+('Viela da Esperança, 606', 'Isabela', 'Female', 'Costa'),
+('Largo da Felicidade, 707', 'João', 'Male', 'Nunes');
