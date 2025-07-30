870abd7412f1efc8b1728d7c9592afe3b56bda06
wkallil <kallil.tim@gmail.com>
2025-07-30 16:57:35 -0300

    refactor: set Json propertys to firstname and lastname

--- src/main/java/br/com/wkallil/data/dto/v1/PersonDTO.java
+++ src/main/java/br/com/wkallil/data/dto/v1/PersonDTO.java
@@ -1,15 +1,21 @@
 package br.com.wkallil.data.dto.v1;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@JsonPropertyOrder({"id", "first_name", "last_name", "address", "gender"})
 public class PersonDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private Long id;
+    @JsonProperty("first_name")
     private String firstName;
+    @JsonProperty("last_name")
     private String lastName;
     private String address;
     private String gender;
