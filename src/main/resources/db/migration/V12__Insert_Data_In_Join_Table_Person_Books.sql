432ff9080b812eba4f986276dfab3d4951edc063
wkallil <kallil.tim@gmail.com>
2025-09-06 15:48:20 -0300

    feat: add new Flyway migrations for books and person_books
    - Adds a new migration to populate the books table
    - Implements migrations to alter the books table, adding `photoUrl` and `wikipediaProfileUrl` columns with preset data
    - Creates a new join table (`person_books`) to manage the relationship between people and books
    - Inserts preset data into the new `person_books` join table

---
+++ src/main/resources/db/migration/V12__Insert_Data_In_Join_Table_Person_Books.sql
@@ -0,0 +1,17 @@
+INSERT INTO person_books (person_id, book_id)
+SELECT 
+    p.id AS person_id, 
+    b.id AS book_id
+FROM 
+    (SELECT id FROM person WHERE id <= 12) p
+CROSS JOIN
+    (SELECT id FROM books ORDER BY RAND() LIMIT 20) b;
+
+INSERT INTO person_books (person_id, book_id)
+SELECT
+    p.id AS person_id,
+    b.id AS book_id
+FROM
+    (SELECT id FROM person WHERE id > 12) p
+CROSS JOIN
+    (SELECT id FROM books ORDER BY RAND() LIMIT 3) b;
