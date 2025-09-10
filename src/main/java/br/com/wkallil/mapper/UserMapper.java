86792092ab355747f199f00ff7fcdeb04eb66189
wkallil <kallil.tim@gmail.com>
2025-09-10 18:03:42 -0300

    refactor: remove unused INSTANCE field
    - Removes the unused INSTANCE field to clean up the code and improve readability

--- src/main/java/br/com/wkallil/mapper/UserMapper.java
+++ src/main/java/br/com/wkallil/mapper/UserMapper.java
@@ -3,12 +3,9 @@ package br.com.wkallil.mapper;
 import br.com.wkallil.data.dto.v1.security.AccountCredentialsDTO;
 import br.com.wkallil.models.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
-    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     User toEntity(AccountCredentialsDTO accountCredentialsDTO);
 
