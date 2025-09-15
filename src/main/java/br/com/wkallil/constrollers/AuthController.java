f843a2afe2bc3004e1869c2292c2b4115fc54f0c
wkallil <kallil.tim@gmail.com>
2025-09-15 17:19:57 -0300

    refactor: switch endpoints to return a token
    - Switches the return type of the signin and refresh endpoints to return a TokenDTO

--- src/main/java/br/com/wkallil/constrollers/AuthController.java
+++ src/main/java/br/com/wkallil/constrollers/AuthController.java
@@ -32,7 +32,7 @@ public class AuthController implements AuthControllerDocs {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
         }
 
-        return ResponseEntity.ok().body(token);
+        return token;
     }
 
     @PutMapping("/refresh/{username}")
@@ -48,7 +48,7 @@ public class AuthController implements AuthControllerDocs {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
         }
 
-        return ResponseEntity.ok().body(token);
+        return token;
     }
 
     @PostMapping(
